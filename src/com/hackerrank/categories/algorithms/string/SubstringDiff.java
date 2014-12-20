4e567ed1073fc2d976d06c820aaa14f1e256347b
tintinmj <anirban.nag9038541773@gmail.com>
2014-12-21 01:54:47 +0530

    Blah

---
+++ src/com/hackerrank/categories/algorithms/string/SubstringDiff.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.hackerrank.categories.algorithms.string;
+
+    /**
+    *
+    * @author tintinmj
+    * problem link https://www.hackerrank.com/challenges/substring-diff
+    * Difficulty : MODERATE
+    */
+public class SubstringDiff {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
